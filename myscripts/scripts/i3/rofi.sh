75a117330a53bfc0e0dd083d11adc900f6ae52e8
mike <mike@nighttype.com>
2016-04-01 11:56:55 -0400

    added compton config, made rofi script with params

---
+++ myscripts/scripts/i3/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#rofi -show run -lines 10 -eh 2 -width 100 -padding 600 -opacity "90" -bw 0 -bc"#282828" -bg "#282828" -fg "$text-color" -hlbg "#282828" -hlfg "#b8bb26" -font "Source Code Pro 15" -separator-style none
+rofi -modi "run,drun" -show run -lines 8 -line-margin 5 -eh 1 -width 700 -padding 25 -location 0 -opacity "90" -bw 0 -bc "#282828" -bg "#282828" -fg "#FFFFFF" -hlbg "#282828" -hlfg "#b8bb26" -font "Source Code Pro Semibold 10"
