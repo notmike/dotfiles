b203820668aa23050ce4716c66e25737eefa466a
mike <mike@nighttype.com>
2016-05-12 23:37:57 -0400

    updated bluetooth scripts

--- myscripts/scripts/connect-bluetooth-headset.sh
+++ myscripts/scripts/connect-bluetooth-headset.sh
@@ -9,13 +9,25 @@ notify-send -h string:bgcolor:#393939 -h string:fgcolor:#99cc99 Bluetooth\ Headp
 pacmd set-sink-mute alsa_output.pci-0000_01_00.1.hdmi-stereo-extra2 1
 
 # run the bluez prog and connect to my earbuds
-echo -e 'power on\nconnect 44:5E:F3:AB:67:A0\nquit' | bluetoothctl
+echo -e 'power on\nquit' | bluetoothctl
 
 # need a little time so default goes thru successfully
-sleep 2  
+sleep 1  
+
+# run the bluez prog and connect to my earbuds
+echo -e 'connect 44:5E:F3:AB:67:A0\nquit' | bluetoothctl
+
+# need a little time so default goes thru successfully
+sleep 3  
 
 # set Volume to something reasonable
 pacmd set-sink-volume bluez_sink.44_5E_F3_AB_67_A0 9830
+
+# set Bluetooth headset as default output
+pacmd set-default-sink bluez_sink.44_5E_F3_AB_67_A0
+
+# need a little time so default goes thru successfully
+sleep 3  
 
 # set Bluetooth headset as default output
 pacmd set-default-sink bluez_sink.44_5E_F3_AB_67_A0
