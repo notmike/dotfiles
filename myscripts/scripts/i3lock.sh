f15a2ad02b10a414dd1ad59a0c66120a018c7223
mike <mike@nighttype.com>
2016-06-26 11:10:00 -0400

    removed the no-fork option on the i3lock since I want system to suspend after locking

--- myscripts/scripts/i3lock.sh
+++ myscripts/scripts/i3lock.sh
@@ -19,5 +19,5 @@ dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPl
 $HOME/scripts/disconnect-bluetooth-headset.sh
 #IMAGE=$HOME/Pictures/Wallpapers/bleeding-mountains.png
 IMAGE=$HOME/Pictures/lockscreen.png
-i3lock -n -i $IMAGE
+i3lock -i $IMAGE
 systemctl suspend
