f819589c2ac1aa5ed58e2e56c1fe1de7445a5d22
mike <mike@nighttype.com>
2016-08-21 02:51:12 -0400

    updated bluetooth script since bluetooth powers on at boot now  ######  increased the blue border around active windows in i3  #####  updated weather i3block script to work in cases where temp is in Celsius or Farenheit

--- myscripts/scripts/connect-bluetooth-headset.sh
+++ myscripts/scripts/connect-bluetooth-headset.sh
@@ -9,10 +9,10 @@ notify-send -h string:bgcolor:#393939 -h string:fgcolor:#99cc99 Bluetooth\ Headp
 pacmd set-sink-mute alsa_output.pci-0000_01_00.1.hdmi-stereo-extra2 1
 
 # run the bluez prog and connect to my earbuds
-echo -e 'power on\nquit' | bluetoothctl
+#echo -e 'power on\nquit' | bluetoothctl
 
 # need a little time so default goes thru successfully
-sleep 1  
+#sleep 1  
 
 # run the bluez prog and connect to my earbuds
 echo -e 'connect 44:5E:F3:AB:67:A0\nquit' | bluetoothctl
