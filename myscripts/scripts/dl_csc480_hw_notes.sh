f195f56311f58da998c03ab8399879f30e09ec04
mike <mike@nighttype.com>
2017-09-21 23:39:12 -0400

    changed script and i3bar icons

--- myscripts/scripts/dl_csc480_hw_notes.sh
+++ myscripts/scripts/dl_csc480_hw_notes.sh
@@ -2,3 +2,4 @@
 #
 # this downloads new pdf files (existing checked against timestamps) from specified site to specified dir
 wget -Nrp -nd -A pdf -e robots=off 'http://www-cs.ccny.cuny.edu/~wes/CSC480F17/docs/lectures/' -P /home/mg/Dropbox/School/City\ College/2017/2017\ Fall/CSC480-CompSecurity/notes
+wget -Nrp -nd -A pdf -e robots=off 'http://www-cs.ccny.cuny.edu/~wes/CSC480F17/docs/hw/' -P /home/mg/Dropbox/School/City\ College/2017/2017\ Fall/CSC480-CompSecurity/hw
