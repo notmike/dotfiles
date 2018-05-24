b3fa299edd0333d1028c375b56916519544e04db
mike <notmike@users.noreply.github.com>
2018-05-24 17:07:24 -0400

    removed command mode to avoid throwing keyboard into command mode when using space cadet shift too quickly.... and reverted to default tapping term (200ms) since i type fast

--- qmk_keymap/qmk_firmware/keyboards/hhkb/keymaps/notmike/config.h
+++ qmk_keymap/qmk_firmware/keyboards/hhkb/keymaps/notmike/config.h
@@ -11,8 +11,8 @@
 #define DESCRIPTION     HHKB on QMK Firmware with notmike keymap
 
 // Increase "Tap" detection window. Avoid missing 'q' or 'z' when typing slowly.
-#undef TAPPING_TERM
-#define TAPPING_TERM 230
+/* #undef TAPPING_TERM */
+/* #define TAPPING_TERM 230 */
 
 // Uncomment to enable NKRO by default. May cause issues with KVM switches.
 //#define FORCE_NKRO
