983b0a6226a9ab53e1273379ceb1692b6c837194
mike <notmike@users.noreply.github.com>
2018-07-17 02:56:25 -0400

    solidified diceware aliases into common

--- zsh/.config/oh-my-zsh/custom/alias_desktop.zsh
+++ zsh/.config/oh-my-zsh/custom/alias_desktop.zsh
@@ -19,18 +19,5 @@ alias mac='macchanger -s enp4s0'
 # polybar config file
 alias polyrc='$EDITOR ~/.config/polybar/config'
 
-# Using diceware python script saved on git will produce diceware pw of specified length.
-# Default word length is 5, then copies to clipboard using xsel (must have installed also)
-alias pw='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 5 | xsel -i -b'
-alias pw2='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 1 | xsel -i -b'
-alias pw3='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 2 | xsel -i -b'
-alias pw4='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 3 | xsel -i -b'
-alias pw5='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 4 | xsel -i -b'
-alias pw6='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 5 | xsel -i -b'
-alias pw7='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 6 | xsel -i -b'
-alias pw8='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 7 | xsel -i -b'
-alias pw9='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 8 | xsel -i -b'
-alias pw10='python2 ~/Dropbox/Programming/git/diceware/diceware-v2 -w 9 | xsel -i -b'
-
 # edit sxhkdrc key-bindings config
 alias sxrc='$EDITOR $HOME/.config/sxhkd/sxhkdrc'
