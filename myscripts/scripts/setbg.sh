359bf9b39f1c2e5dd1a3923d2810b005afd34376
mike <notmike@users.noreply.github.com>
2018-08-03 02:20:23 -0400

    new bg

--- myscripts/scripts/setbg.sh
+++ myscripts/scripts/setbg.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # script will set the wallpaper
-feh --bg-fill '/home/mg/Pictures/Wallpapers/beach-dunes.jpg'
+feh --bg-tile '/home/mg/Pictures/Wallpapers/patterns/pink_dust.png'
 #feh --bg-fill '/home/mg/Pictures/wallpaper.png'
-#feh --bg-fill '/home/mg/Pictures/Wallpapers/bleeding-mountains.png'
