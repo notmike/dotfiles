397bb05cd36e40911aef5e6215705e826591fee2
mike <notmike@users.noreply.github.com>
2018-10-26 02:40:44 -0400

    polybar script update

--- polybar/.config/polybar/ip-template.sh
+++ polybar/.config/polybar/ip-template.sh
@@ -5,7 +5,7 @@ realip="PUT YOUR REAL IP ADDRESS HERE!"
 addr="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 if [[ $realip = $addr ]]; then  # VPN OFF!
-    echo "%{F#fb4934}$addr"     # red text
+    echo "%{F#fb4934} $addr"     # red text
 else
-    echo "$addr"
+    echo " $addr"
 fi
