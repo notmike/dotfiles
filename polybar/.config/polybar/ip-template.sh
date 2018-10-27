33d17a73b216fe3a4743614e75ad982f63ab4602
mike <notmike@users.noreply.github.com>
2018-10-27 00:28:17 -0400

    polybar: adjusting laptop scripts

--- polybar/.config/polybar/ip-template.sh
+++ polybar/.config/polybar/ip-template.sh
@@ -4,8 +4,14 @@ realip="PUT YOUR REAL IP ADDRESS HERE!"
 
 addr="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-if [[ $realip = $addr ]]; then  # VPN OFF!
-    echo "%{F#fb4934} $addr"     # red text
+if [[ $addr =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+
+    if [[ $realip = $addr ]]; then  # VPN OFF!
+        echo "%{F#fb4934} $addr"     # red text
+    else
+        echo " $addr"
+    fi
+
 else
-    echo " $addr"
+    echo "%{F#d65d0e} $addr"     # red text
 fi
