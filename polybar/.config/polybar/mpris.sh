1673ea1ce5b28fb90f6ae4b06359137ce51215c6
mike <notmike@users.noreply.github.com>
2020-07-05 21:44:04 -0400

    regular updates

--- polybar/.config/polybar/mpris.sh
+++ polybar/.config/polybar/mpris.sh
@@ -3,10 +3,10 @@
 # Specifying the icon(s) in the script
 # This allows us to change its appearance conditionally
 icon=" "
-player_status="$(playerctl status 2> /dev/null)"
+player_status="$(playerctl -p spotify status 2> /dev/null)"
 
-if [[ $? -eq 0 && $(playerctl -l) == *"spotify"* ]]; then
-    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+if [[ $? -eq 0 ]]; then
+    metadata="$(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
     # Foreground color formatting tags are optional
     if [[ $player_status = "Playing" ]]; then
         echo "%{F#8ec07c}$icon %{F#b8bb26}$metadata"    # Green when playing
