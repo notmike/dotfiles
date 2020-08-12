369e666ec7fbdb8fb675d31860a1d8678e7d481d
mike <notmike@users.noreply.github.com>
2020-08-12 01:48:55 -0400

    qmk/i3config/ranger/rofi/roficlip/qbittorrent reg updates

--- qmk_keymap/qmk_firmware/keyboards/hhkb/keymaps/notmike/keymap.c
+++ qmk_keymap/qmk_firmware/keyboards/hhkb/keymaps/notmike/keymap.c
@@ -11,6 +11,7 @@
 #define HHKB 2
 #define UTIL 3
 #define MREC 4
+#define DBUG 5
 
 // Easier-to-read Layer Arrays.
 #define ____ KC_TRNS
@@ -43,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWER] = LAYOUT(
   KC_ESC,  KC_1,           KC_2,    KC_3,             KC_4,    KC_5,     KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,        KC_BSLS, KC_GRV, \
   KC_TAB,  LT(MREC, KC_Q), KC_W,    KC_E,             KC_R,    KC_T,     KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,       KC_BSPC,         \
-  KC_LCTL, KC_A,           KC_S,    KC_D,             KC_F,    KC_G,     KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,                  \
+  KC_LCTL, KC_A,           KC_S,    LT(DBUG, KC_D),   KC_F,    KC_G,     KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,                  \
   KC_LSPO, KC_Z,           KC_X,    KC_C,             KC_V,    KC_B,     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, MO(HHKB),                       \
            KC_LALT,        KC_LGUI, LT(UTIL, KC_SPC), KC_RGUI, KC_RALT),
 
@@ -97,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* UTIL Layer: Extra utilities
      *
      * ,-------------------------------------------------------------.
-     * |DFU|PLY1|PLY2|   |   |   |   |   |   |   |   |QWE|GME|UTL|DBG|
+     * |   |PLY1|PLY2|   |   |   |   |DFU|   |   |   |   |GME|UTL|DBG|
      * |-------------------------------------------------------------|
      * |     |    |MLB|M-Up|MRB|MwU|Hom|PgD|PgU|End|Psc|   |   |     |
      * |-------------------------------------------------------------|
@@ -111,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
   [UTIL] = LAYOUT(
-  RESET, DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, ____,    ____,    ____,   ____,    ____,    ____,    ____,    ____, ____, TO(GAME), DF(UTIL), DEBUG, \
+  ____, DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, ____,    ____,    ____,   ____,    RESET,    ____,    ____,    ____, ____, TO(GAME), DF(UTIL), DEBUG, \
   ____,  ____,  KC_BTN1,  KC_MS_U,  KC_BTN2, KC_WH_U, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_PSCR, ____,     ____,     KC_DEL,        \
   ____,  ____,  KC_MS_L,  KC_MS_D,  KC_MS_R, KC_WH_D, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ____, ____,     ____,                  \
   ____,  ____,   ____,    ____,    ____,    KC_SPC, ____,    ____,    ____,    ____,    ____, ____,     ____,                  \
@@ -140,8 +141,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ____, ____,           ____,           ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       \
   ____, ____,           DYN_REC_STOP,   ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             \
   ____, ____,           ____,           ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             \
-        ____,           ____,           ____, ____, ____)
+        ____,           ____,           ____, ____, ____),
 
+
+    /* DEBUG Layer: Made to map debugging shortcuts to Fn keys. Can be repurposed.
+     *
+     * ,-------------------------------------------------------------.
+     * |   |    |    |   |   |   |   |   |   |   |   |   |   |   |   |
+     * |-------------------------------------------------------------|
+     * |     |    |    |   |F1 |   |   |F2 |F3 |F4 |F5 |   |   |     |
+     * |-------------------------------------------------------------|
+     * |      |    | F6 |   | F7|   | F8| F9|F10|F11|   |   |        |
+     * |-------------------------------------------------------------|
+     * |        |    |    |   |   |   |   |   |   |   |   |      |   |
+     * `-------------------------------------------------------------'
+     *       |   |     |                       |     |   |
+     *       `-------------------------------------------'
+     *
+     */
+
+  [DBUG] = LAYOUT(
+  ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, \
+  ____, ____, ____,   ____, KC_F1, ____, ____, KC_F2, KC_F3, KC_F4, KC_F5, ____, ____, ____,       \
+  ____, ____, KC_F6, ____, KC_F7, ____, KC_F8, KC_F9, KC_F10, KC_F11, ____, ____, ____,             \
+  ____, ____, ____,   ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             \
+        ____, ____,   ____, ____, ____)
 };
 
 
