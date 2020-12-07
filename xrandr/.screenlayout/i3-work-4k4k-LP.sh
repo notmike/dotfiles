35e116d2ac606b1a89ce6d2d2f8a6b54bfe87b9c
mike <notmike@users.noreply.github.com>
2020-12-07 08:27:43 -0500

    update monitor setup

--- xrandr/.screenlayout/i3-work-4k4k-LP.sh
+++ xrandr/.screenlayout/i3-work-4k4k-LP.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate right --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --pos 0x725 --rotate normal --output DP-5 --off
+sleep 3
+i3 restart
