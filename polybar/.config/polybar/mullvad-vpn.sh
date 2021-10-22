9ca17668b5d960c30838920fdba5e8f927ca91ce
mike <notmike@users.noreply.github.com>
2021-10-22 11:05:16 -0400

    vpn updates

---
+++ polybar/.config/polybar/mullvad-vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+addr="$(curl -sf ifconfig.co)"
+vpnstatus="$(mullvad status)"
+disconnected='Disconnected'
+
+if [[ "$vpnstatus" == *"$disconnected"* ]]; then  # VPN OFF!
+    echo "%{F#fb4934} $addr"     # red text
+
+    # Kill qBittorrent if VPN is OFF
+    torrent="$(pgrep qbittorrent)"
+    if [[ $torrent -gt 0 ]]; then
+        pkill -9 qbittorrent
+        notify-send -u critical -t 30000 "KILLING qBittorrent!
+    KILLING qBittorrent!
+    KILLING qBittorrent!
+    KILLING qBittorrent!
+    KILLING qBittorrent!"
+    fi
+else
+    echo " $addr"
+fi
+
